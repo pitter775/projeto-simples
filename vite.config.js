38dff551a465c373d546b57c76f2cac111153789
Pitter <pitter775@gmail.com>
2025-01-11 18:11:40 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -16,9 +16,5 @@ export default defineConfig({
         host: '0.0.0.0', // Garantir que o host seja acessível
         port: 8080, // Você pode ajustar para a porta usada no Railway
     },
-    build: {
-        manifest: true, // Gera o arquivo manifest para assets
-        outDir: 'public/build', // Diretório onde os assets compilados serão salvos
-    },
 });
 
